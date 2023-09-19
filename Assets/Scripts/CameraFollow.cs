72ba8ca460564918c98165d03294b391c2252718
Alex Bacon <abacon2@academic.rrc.ca>
2023-09-19 11:59:05 -0500

    added camera follow movement to the main character object

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public GameObject objectToFollow;
+    public Vector3 objectPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectPos = objectToFollow.transform.position;
+        gameObject.transform.position = objectPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.transform.position = new Vector3(objectToFollow.transform.position.x, objectToFollow.transform.position.y, objectToFollow.transform.position.z - 10); //offset Z so that you can see objects
+
+
     }
 }
