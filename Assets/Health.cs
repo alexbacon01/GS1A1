7c639c9fceb08f2a048ae4762fe47bb8c4df3b50
Alex Bacon <abacon2@academic.rrc.ca>
2023-09-26 12:28:23 -0500

    Revert "changed health broke movement"
    This reverts commit a4841d1a38c494588a39cb93a4e8cea0613e09ab.

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -31,7 +31,6 @@ public class Health : MonoBehaviour
         if (collision)
         {
             currentHealth += changeAmount;
-
             updateHealthBar();
            
             collision = false;
