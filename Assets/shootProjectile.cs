9fc58e5e5f8a91f1ac8488932501ddbcd78b561d
Alex Bacon <abacon2@academic.rrc.ca>
2023-09-27 12:03:18 -0500

    enemy ship detects player in radius and shoots projectile, shooting is buggy

---
+++ Assets/shootProjectile.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootProjectile : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Rigidbody2D rb; //the rigid body of projectile
+    public float forceAmount = 1f; //force amount
+    public GameObject target; //game object for target
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //get componenet rigid body
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    private void FixedUpdate()
+    {
+        if (rb != null) //if rb is not null
+        {
+            MoveProjectile(); //move the projectile
+        }
+    }
+
+    //function to move the projectile
+    void MoveProjectile()
+    {
+        rb.AddForce(transform.up * forceAmount, ForceMode2D.Impulse); //add force with impulse to shoot it instantly
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<SpriteRenderer>().color = Color.red; //when it collides with object change colour to red 
+        Debug.Log(collision.gameObject.name); //what did it hit??
+    }
+}
